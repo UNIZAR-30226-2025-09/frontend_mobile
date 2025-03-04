fca228f3d536214a4ccd17b3f743f99ab3237948
874095 <874095@unizar.es>
2025-03-04 12:48:28 +0100

    Re-Init

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "kotlin-basic-skeleton"
--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++
@@ -1,9 +0,0 @@
-package com.codely.demo
-
-class Codelyber {
-    fun greet() = "Welcome to kotlin skeleton!!!"
-}
-
-fun main() {
-    println(Codelyber().greet())
-}
