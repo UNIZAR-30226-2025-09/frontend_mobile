656c1bc07ec2d81651c642babb5d6ffdab3def4d
Marcos <874095@unizar.es>
2025-03-06 16:47:45 +0100

    Estructura Basica App Navegacion

--- app/src/main/java/eina/unizar/es/ui/main/MainScreen.kt
+++ app/src/main/java/eina/unizar/es/ui/main/MainScreen.kt
@@ -1,4 +1,43 @@
 package eina.unizar.es.ui.main
 
-class MainScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bienvenido a Vibra",
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Custom button with a theme
+        Button(
+            onClick = { navController.navigate("player") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue, // Custom background color
+                contentColor = Color.White// Custom text color
+            ),
+            shape = MaterialTheme.shapes.medium, // Custom shape
+            modifier = Modifier.padding(8.dp) // Padding for the button
+        ) {
+            Text(text = "Comenzar")
+        }
+    }
 }
---
+++ app/src/main/java/eina/unizar/es/ui/navigation/AppNavigator.kt
@@ -0,0 +1,37 @@
+package eina.unizar.es.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import eina.unizar.es.ui.main.MainScreen
+import eina.unizar.es.ui.player.PlayerScreen
+
+@Composable
+fun AppNavigator() {
+    val navController: NavHostController = rememberNavController()
+
+    Scaffold { innerPadding ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            NavHost(
+                navController = navController,
+                startDestination = "main",
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable("main") { MainScreen(navController) }
+                composable("player") { PlayerScreen() }
+            }
+        }
+    }
+}
+
+
+
+
+
