c70a6edc30c7d7e20a4de86c605ade1b9cb7a4a0
marietuco11 <marietuco11@gmail.com>
2025-03-22 18:35:26 +0100

    Creacion de listas de reproduccion desde la pantalla de la biblioteca

--- app/src/main/java/eina/unizar/es/ui/library/LibraryScreen.kt
+++ app/src/main/java/eina/unizar/es/ui/library/LibraryScreen.kt
@@ -1,5 +1,7 @@
 package eina.unizar.es.ui.library
 
+import android.app.LauncherActivity
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,9 +24,24 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.musicapp.ui.theme.VibraBlack
+import com.example.musicapp.ui.theme.VibraBlue
+import com.example.musicapp.ui.theme.VibraLightGrey
+import com.example.musicapp.ui.theme.VibraWhite
 import eina.unizar.es.R
+import eina.unizar.es.data.model.network.ApiClient.get
+import eina.unizar.es.data.model.network.ApiClient.post
+import eina.unizar.es.ui.playlist.Playlist
+import eina.unizar.es.ui.song.Song
 import eina.unizar.es.ui.user.UserProfileMenu
 import eina.unizar.es.ui.theme.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,10 +49,10 @@ fun LibraryScreen(navController: NavController) {
     var searchText by remember { mutableStateOf(TextFieldValue("")) } // Estado del texto de búsqueda
     var isSearching by remember { mutableStateOf(false) } // Estado para mostrar la barra de búsqueda
 
-    val allLibraryItems = getLibraryItems("Playlists") // Lista completa
-    val filteredLibraryItems = allLibraryItems.filter {
-        it.name.contains(searchText.text, ignoreCase = true)
-    }
+    var showCreatePlaylistDialog by remember { mutableStateOf(false) }
+    var newPlaylistName by remember { mutableStateOf("") }
+
+
 
     // Estado de la barra de navegación inferior
     var selectedItem by remember { mutableStateOf(2) } // 2 es "Biblioteca" por defecto
@@ -45,6 +62,81 @@ fun LibraryScreen(navController: NavController) {
         Pair("Buscar", Icons.Default.Search),
         Pair("Biblioteca", Icons.Rounded.Menu)
     )
+
+    var playlists by remember { mutableStateOf<List<Playlist>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        val response = get("playlists") // Llamada a la API
+        response?.let {
+            val jsonArray = JSONArray(it)
+            val fetchedPlaylists = mutableListOf<Playlist>()
+
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                fetchedPlaylists.add(
+                    Playlist(
+                        id = jsonObject.getString("id"),
+                        title = jsonObject.getString("name"),
+                        idAutor = jsonObject.getString("user_id"),
+                        idArtista = jsonObject.getString("artist_id"),
+                        description = jsonObject.getString("description"),
+                        esPublica = jsonObject.getString("type"),
+                        esAlbum = jsonObject.getString("typeP"),
+                        imageUrl = jsonObject.getString("front_page")
+                    )
+                )
+            }
+            playlists = fetchedPlaylists
+        }
+    }
+
+    suspend fun createPlaylist(playlistName: String) {
+        val jsonBody = JSONObject().apply {
+            put("name", playlistName)
+            // Puedes agregar más campos si es necesario
+        }
+
+        coroutineScope  { // Lanza una corrutina en el scope
+            val response = withContext(Dispatchers.IO) { // Cambia al hilo IO para la red
+                post("playlists", jsonBody)
+            }
+
+            response?.let {
+                // Manejar la respuesta del servidor (si es necesario)
+                println("Playlist creada con éxito: $it")
+
+                // Actualizar la lista de playlists después de crear una nueva
+                val getResponse = withContext(Dispatchers.IO) {
+                    get("playlists")
+                }
+
+                getResponse?.let {
+                    val jsonArray = JSONArray(it)
+                    val fetchedPlaylists = mutableListOf<Playlist>()
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        fetchedPlaylists.add(
+                            Playlist(
+                                id = jsonObject.getString("id"),
+                                title = jsonObject.getString("name"),
+                                idAutor = jsonObject.getString("user_id"),
+                                idArtista = jsonObject.getString("artist_id"),
+                                description = jsonObject.getString("description"),
+                                esPublica = jsonObject.getString("type"),
+                                esAlbum = jsonObject.getString("typeP"),
+                                imageUrl = jsonObject.getString("front_page")
+                            )
+                        )
+                    }
+                    playlists = fetchedPlaylists
+                }
+            } ?: run {
+                // Manejar el error si la creación de la playlist falla
+                println("Error al crear la playlist")
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -111,10 +203,16 @@ fun LibraryScreen(navController: NavController) {
         ) {
             // Si la búsqueda está activa, mostramos el cuadro de búsqueda debajo del perfil
             if (isSearching) {
+
                 OutlinedTextField(
                     value = searchText,
                     onValueChange = { searchText = it },
-                    placeholder = { Text("Buscar en tu biblioteca", color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)) },
+                    placeholder = {
+                        Text(
+                            "Buscar en tu biblioteca",
+                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                        )
+                    },
                     textStyle = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onBackground),
                     shape = RoundedCornerShape(16.dp),
                     modifier = Modifier
@@ -140,62 +238,134 @@ fun LibraryScreen(navController: NavController) {
                     }
                 )
             }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Tu Biblioteca",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
 
-            // Título "Tu Biblioteca"
-            Text(
-                text = "Tu Biblioteca",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-            )
+                Button(
+                    onClick = { showCreatePlaylistDialog = true },
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = VibraBlue),
+                    modifier = Modifier.height(40.dp)
+                ) {
+                    Text("Crear Playlist", color = VibraWhite)
+                }
+            }
+
 
             // Lista de elementos filtrados según la búsqueda
             LazyColumn(modifier = Modifier.padding(8.dp)) {
-                items(filteredLibraryItems) { item ->
+                items(playlists) { item ->
                     LibraryItem(item)
                 }
             }
+        }
+        // Pop-up para crear playlist
+        if (showCreatePlaylistDialog) {
+            AlertDialog(
+                onDismissRequest = { showCreatePlaylistDialog = false },
+                title = {
+                    Text(
+                        "Nombre de la playlist",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }, // OnBackground
+                text = {
+                    OutlinedTextField(
+                        value = newPlaylistName,
+                        onValueChange = { newPlaylistName = it },
+                        label = {
+                            Text(
+                                "Nombre",
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }, // onSurface
+                        singleLine = true,
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = MaterialTheme.colorScheme.primary,
+                            unfocusedBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                            cursorColor = MaterialTheme.colorScheme.primary
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                confirmButton = {
+                    Row(horizontalArrangement = Arrangement.Start) { // Alineamos a la izquierda
+                        Spacer(modifier = Modifier.width(48.dp)) // Agregamos un margen izquierdo
+                        Button(
+                            onClick = {
+                                if (newPlaylistName.isNotEmpty()) {
+                                    CoroutineScope(Dispatchers.Main).launch {
+                                        createPlaylist(newPlaylistName)
+                                    }
+                                    showCreatePlaylistDialog = false
+                                }
+                            },
+                            shape = RoundedCornerShape(16.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if (newPlaylistName.isNotEmpty()) VibraBlue else VibraLightGrey
+                            ),
+                            enabled = newPlaylistName.isNotEmpty()
+                        ) {
+                            Text("Confirmar", color = VibraWhite)
+                        }
+                    }
+                },
+                dismissButton = {
+                    Row(horizontalArrangement = Arrangement.Start) { // Alineamos a la izquierda
+                        Spacer(modifier = Modifier.width(0.dp)) // Agregamos un margen izquierdo
+                        Button(
+                            onClick = { showCreatePlaylistDialog = false },
+                            colors = ButtonDefaults.buttonColors(containerColor = VibraLightGrey)
+                        ) {
+                            Text("Cancelar", color = VibraBlack)
+                        }
+                    }
+                }
+            )
         }
     }
 }
 
 @Composable
-fun LibraryItem(item: LibraryItem) {
+fun LibraryItem(playlist: Playlist) { // Modificamos para usar Playlist
     Row(
         modifier = Modifier.fillMaxWidth().padding(8.dp).clickable {},
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            painter = painterResource(id = R.drawable.kanyeperfil),
+            painter = painterResource(id = R.drawable.kanyeperfil), // Puedes usar playlist.imageUrl si lo tienes
             contentDescription = "Imagen",
             modifier = Modifier.size(50.dp)
         )
         Spacer(modifier = Modifier.width(10.dp))
         Column {
             Text(
-                text = item.name,
+                text = playlist.title, // Usamos playlist.title
                 style = MaterialTheme.typography.bodyLarge,
                 color = MaterialTheme.colorScheme.onBackground
             )
-            Text(
-                text = item.type,
-                style = MaterialTheme.typography.labelLarge,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
-            )
+            Log.d("Descripciones", playlist.description)
+                Text(
+                    text = if (playlist.description != "null") {
+                        playlist.description
+                    } else {
+                        "Añade aquí una descripción"
+                    },
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
+
         }
     }
 }
 
-
-// Datos simulados
-data class LibraryItem(val name: String, val type: String)
-
-fun getLibraryItems(filter: String): List<LibraryItem> {
-    return when (filter) {
-        "Playlists" -> listOf(
-            LibraryItem("Canciones que te gustan", "Lista"),
-            LibraryItem("Vibra", "Lista")
-        )
-        else -> emptyList()
-    }
-}
