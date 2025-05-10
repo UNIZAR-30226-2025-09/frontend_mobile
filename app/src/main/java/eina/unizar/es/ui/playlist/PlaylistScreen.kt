838667fd8c2fa16d4f4cc1e22b71d02fd07b34ae
carlosalejoss <872342@unizar.es>
2025-05-10 11:51:12 +0200

    quitados imports innecesarios y comentado botón de Abrir Vibra App al compartir una playlist

--- app/src/main/java/eina/unizar/es/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/eina/unizar/es/ui/playlist/PlaylistScreen.kt
@@ -1,36 +1,28 @@
 package eina.unizar.es.ui.playlist
 
-import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.util.Log
-import android.widget.Space
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
@@ -48,16 +40,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathSegment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -65,16 +54,12 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import eina.unizar.es.R
 import eina.unizar.es.data.model.network.ApiClient.get
 import eina.unizar.es.data.model.network.ApiClient.delete
 import eina.unizar.es.ui.song.Song
-import org.json.JSONArray
 import org.json.JSONObject
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
 import eina.unizar.es.data.model.network.ApiClient.getLikedPlaylists
 import eina.unizar.es.data.model.network.ApiClient.getUserData
 import eina.unizar.es.data.model.network.ApiClient.likeUnlikePlaylist
@@ -83,32 +68,22 @@ import coil.compose.AsyncImage
 import com.example.musicapp.ui.theme.VibraBlack
 import com.example.musicapp.ui.theme.VibraBlue
 import com.example.musicapp.ui.theme.VibraLightGrey
-import com.stripe.android.core.strings.resolvableString
-import eina.unizar.es.data.model.network.ApiClient
 import eina.unizar.es.data.model.network.ApiClient.checkIfSongIsLiked
 import eina.unizar.es.data.model.network.ApiClient.getImageUrl
-import eina.unizar.es.data.model.network.ApiClient.getLikedPlaylists
 import eina.unizar.es.data.model.network.ApiClient.getLikedSongsPlaylist
 import eina.unizar.es.data.model.network.ApiClient.getSongDetails
-import eina.unizar.es.data.model.network.ApiClient.getUserData
 import eina.unizar.es.data.model.network.ApiClient.isPlaylistOwner
-import eina.unizar.es.data.model.network.ApiClient.likeUnlikePlaylist
 import eina.unizar.es.data.model.network.ApiClient.likeUnlikeSong
 import eina.unizar.es.data.model.network.ApiClient.post
 import eina.unizar.es.data.model.network.ApiClient.recordPlaylistVisit
 import eina.unizar.es.data.model.network.ApiClient.updatePlaylist
 import eina.unizar.es.data.model.network.ApiClient.togglePlaylistType
 import eina.unizar.es.ui.artist.SongOptionItem
-import eina.unizar.es.ui.navbar.BottomNavigationBar
-import eina.unizar.es.ui.player.FloatingMusicPlayer
 import eina.unizar.es.ui.player.MusicPlayerViewModel
 import eina.unizar.es.ui.search.SongItem
 import eina.unizar.es.ui.search.convertSongsToCurrentSongs
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.nio.file.Files.delete
 import eina.unizar.es.ui.artist.SongOptionsBottomSheetContent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
@@ -1092,14 +1067,14 @@ fun BottomSheetContent(
                 Text("de $playlistAuthor", color = Color.Gray, fontSize = 12.sp)
             }
 
-            Button(
-                onClick = {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vibra://playlist/3"))
-                    context.startActivity(intent)
-                }
-            ) {
-                Text("Abrir Vibra App")
-            }
+//            Button(
+//                onClick = {
+//                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vibra://playlist/3"))
+//                    context.startActivity(intent)
+//                }
+//            ) {
+//                Text("Abrir Vibra App")
+//            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
